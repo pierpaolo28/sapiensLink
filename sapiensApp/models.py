faaf3c3d35ac05979ea233978fa0789501e1a832
pierpaolo28 <40637715+pierpaolo28@users.noreply.github.com>
2023-09-14 16:17:42 +0200

    host to author remanimng

--- sapiensApp/models.py
+++ sapiensApp/models.py
@@ -64,7 +64,7 @@ class Topic(models.Model):
 
 
 class List(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     content = models.TextField(null=True, blank=True)
