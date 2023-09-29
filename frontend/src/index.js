751abff994f7902ce6a056a15b7e4f44a84647a8
Lorenzo <lorenzozar@gmail.com>
2023-09-29 18:17:47 +0300

    And & config Mantine UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MantineProvider, createTheme } from "@mantine/core";
+import "@mantine/core/styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MantineProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MantineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
