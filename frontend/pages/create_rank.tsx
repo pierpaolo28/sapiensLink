ca9f27514c6e13ccd17b212efb255ca05e583ec2
pierpaolo28 <40637715+pierpaolo28@users.noreply.github.com>
2024-01-13 10:52:13 +0100

    create rank updated user experience

--- frontend/pages/create_rank.tsx
+++ frontend/pages/create_rank.tsx
@@ -9,7 +9,9 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import IconButton from '@mui/material/IconButton';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CheckIcon from '@mui/icons-material/Check';
 import Select from '@mui/material/Select';
 import { SelectChangeEvent } from '@mui/material/Select';
 
@@ -34,6 +36,9 @@ const CreateRankForm = () => {
     content: {},
   });
   const [error, setError] = React.useState<string | null>(null);
+  const [currentElement, setCurrentElement] = React.useState('');
+  const [editingElementIndex, setEditingElementIndex] = React.useState<number | null>(null);
+  const [currentEditedElement, setCurrentEditedElement] = React.useState('');
 
   React.useEffect(() => {
     // Check if the user is logged in
@@ -47,7 +52,7 @@ const CreateRankForm = () => {
     const selectedTopicsData = (event.target.value as string[]).map((topicName) => ({ name: topicName }));
     setSelectedTopics(event.target.value as string[]);
     handleFormChange('topic', selectedTopicsData);
-  };  
+  };
 
   const handleFormChange = (field: keyof CreateRankFormData, value: any) => {
     if (field === 'name') {
@@ -64,29 +69,13 @@ const CreateRankForm = () => {
       }));
     }
   };
-  
-
-  const addElement = () => {
-    const newElements = [...elements];
-    const newContent: { [key: string]: ContentItem } = { ...formData.content };
-    const key = String.fromCharCode('a'.charCodeAt(0) + newElements.length);
-
-    newElements.push('');
-    newContent[key] = {
-      element: '',
-      user_id: getUserIdFromAccessToken(),
-    };
-
-    setElements(newElements);
-    handleFormChange('content', newContent);
-  };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-  
+
     // Reset error state on each submission attempt
     setError(null);
-  
+
     // Check if the mandatory fields are filled
     if (formData.name && formData.topic && formData.topic.length > 0) {
       try {
@@ -99,7 +88,7 @@ const CreateRankForm = () => {
           },
           body: JSON.stringify(formData),
         });
-  
+
         if (response.ok) {
           window.location.href = '/rank_home';
         } else {
@@ -114,7 +103,7 @@ const CreateRankForm = () => {
               // Display a user-friendly error message for similar ranks
               setError(`Similar ranks found. Please check the existing ranks: ${responseData.similar_ranks.map((rank: any) => (
                 '<a key=' + rank.id + ' href="/rank?id=' + rank.id + '">' + rank.name + '</a>'
-              )).join(', ')}`);            
+              )).join(', ')}`);
             } else {
               setError(responseData.message || 'Failed to submit the form');
             }
@@ -133,46 +122,76 @@ const CreateRankForm = () => {
   };
 
 
-const removeElement = (indexToRemove: number) => {
-  const newElements = elements.filter((_, index) => index !== indexToRemove);
-  const newContent: { [key: string]: ContentItem } = {};
+  const removeElement = (indexToRemove: number) => {
+    const newElements = elements.filter((_, index) => index !== indexToRemove);
+    const newContent: { [key: string]: ContentItem } = {};
 
-  newElements.forEach((value, index) => {
-    const key = String.fromCharCode('a'.charCodeAt(0) + index);
-    newContent[key] = {
-      element: value,
-      user_id: getUserIdFromAccessToken(),
-    };
-  });
+    newElements.forEach((value, index) => {
+      const key = String.fromCharCode('a'.charCodeAt(0) + index);
+      newContent[key] = {
+        element: value,
+        user_id: getUserIdFromAccessToken(),
+      };
+    });
 
-  setElements(newElements);
-  handleFormChange('content', newContent);
-};
-
-  const handleElementChange = (index: number, value: string) => {
-    const newElements = [...elements];
-    newElements[index] = value;
-  
-    const newContent: { [key: string]: ContentItem } = { ...formData.content };
-    const key = String.fromCharCode('a'.charCodeAt(0) + index);
-  
-    newContent[key] = {
-      element: value,
-      user_id: getUserIdFromAccessToken(),
-    };
-  
     setElements(newElements);
     handleFormChange('content', newContent);
   };
-  
+
   const handleQuillKeyDown = (event: any) => {
     if (event.key === 'Enter') {
       event.preventDefault();
       addElement();
     }
   };
-  
-  
+
+  const handleEditQuillKeyDown = (event: any) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      saveEditedElement();
+    }
+  };
+
+  const addElement = () => {
+    // Check if the currentElement is not just empty or whitespace
+    console.log(currentElement)
+    if (currentElement !== '<p><br></p><p><br></p>') {
+      // Sanitize and add the element as before
+      let sanitizedElement = currentElement
+        .replace(/<br>/g, '')
+        .replace(/<\/p><p>/g, '')
+        .replace(/<p><br><\/p>$/, '');
+
+      const newElements = [...elements, sanitizedElement];
+      setElements(newElements);
+      setCurrentElement('');
+    } else {
+      // Optionally, provide feedback to the user that input is required
+      alert('Please enter some text before adding an element.');
+    }
+  };
+
+
+  const saveEditedElement = () => {
+    // Now save the edited element value to the elements array
+    const updatedElements = elements.map((element, index) => {
+      if (index === editingElementIndex) {
+        let sanitizedElement = currentEditedElement
+          .replace(/<br>/g, '')
+          .replace(/<\/p><p>/g, '')
+          .replace(/<p><br><\/p>$/, '');
+        return sanitizedElement;
+      }
+      return element;
+    });
+
+    setElements(updatedElements);
+    // Clear the edited values and close the editor
+    setCurrentEditedElement('');
+    setEditingElementIndex(null);
+  };
+
+
   return (
     <AppLayout>
       <Container maxWidth="md" sx={{ mt: 4 }}>
@@ -181,11 +200,11 @@ const removeElement = (indexToRemove: number) => {
         </Typography>
         {error && (
           <Typography
-          variant="body1"
-          color="error"
-          gutterBottom
-          dangerouslySetInnerHTML={{ __html: error }}
-        />        
+            variant="body1"
+            color="error"
+            gutterBottom
+            dangerouslySetInnerHTML={{ __html: error }}
+          />
         )}
         <Box component="form" noValidate autoComplete="off" onSubmit={handleSubmit}>
           <TextField
@@ -210,41 +229,52 @@ const removeElement = (indexToRemove: number) => {
             value={formData.description}
             onChange={(e) => handleFormChange('description', e.target.value)}
           />
-          {elements.map((element, index) => (
-            <Box key={index} sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
-            <ReactQuill
-              value={element}
-              onChange={(value) => handleElementChange(index, value)}
-              onKeyDown={(event) => handleQuillKeyDown(event)}
-              modules={{
-                toolbar: [
-                  [{ 'header': [1, 2, false] }],
-                  ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                  [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-                  ['link'],
-                  ['clean'],
-                ],
-              }}
-              formats={[
-                'header',
-                'bold', 'italic', 'underline', 'strike', 'blockquote',
-                'list', 'bullet', 'indent',
-                'link',
-              ]}
-              theme="snow"
-            />
-            {elements.length > 1 && (
-      <IconButton onClick={() => removeElement(index)} color="secondary">
-        <RemoveCircleOutlineIcon />
-      </IconButton>
-    )}
-    {index === elements.length - 1 && (
-      <IconButton onClick={addElement} color="primary">
-        <AddCircleOutlineIcon />
-      </IconButton>
-    )}
+          <Box sx={{ display: 'flex', alignItems: 'center', mt: 2, width: '100%' }}>
+            <Box sx={{ flexGrow: 1, mr: 1 }}> {/* flexGrow allows the box to expand, mr adds some margin to the right */}
+              <ReactQuill
+                value={currentElement}
+                onChange={setCurrentElement}
+                onKeyDown={handleQuillKeyDown}
+                theme="snow"
+              />
+            </Box>
+            <IconButton onClick={addElement} color="primary" aria-label="add" sx={{ flexShrink: 0 }}> {/* flexShrink ensures the button doesn't shrink */}
+              <AddCircleOutlineIcon />
+            </IconButton>
           </Box>
-          ))}
+
+          {/* Elements list */}
+          {elements.map((element, index) => (
+            element && (
+              <Box key={index} sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
+                {editingElementIndex === index ? (
+                  <>
+                    <ReactQuill
+                      value={currentEditedElement}
+                      onChange={(value) => setCurrentEditedElement(value)}
+                      onKeyDown={(event) => handleEditQuillKeyDown(event)}
+                      theme="snow"
+                    />
+                    <IconButton onClick={() => saveEditedElement()} color="primary">
+                      <CheckIcon />
+                    </IconButton>
+                  </>
+                ) : (
+                  <>
+                    <Typography dangerouslySetInnerHTML={{ __html: element }} />
+                    <IconButton onClick={() => {
+                      setCurrentEditedElement(element);
+                      setEditingElementIndex(index);
+                    }} color="default">
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton onClick={() => removeElement(index)} color="secondary">
+                      <DeleteIcon />
+                    </IconButton>
+                  </>
+                )}
+              </Box>
+            )))}
           <FormControl required fullWidth margin="normal" variant="outlined" sx={{ mt: 2 }}>
             <InputLabel id="topic-label">Topic</InputLabel>
             <Select
