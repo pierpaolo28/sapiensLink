4fe76c020e1d17c7d28dbc007d6a0a077c8545af
pierpaolo28 <40637715+pierpaolo28@users.noreply.github.com>
2024-01-20 16:24:45 +0100

    vision update

--- frontend/pages/vision.tsx
+++ frontend/pages/vision.tsx
@@ -9,25 +9,29 @@ import AppLayout from "@/components/AppLayout";
 const VisionPage: React.FC = () => {
   return (
     <AppLayout>
-    <Container component="main" sx={{ flexGrow: 1, p: 3, mt: 3 }}>
-      <Head>
-        <title>Our Vision</title>
-        <meta name="description" content="Learn more about SapiensLink vision." />
-      </Head>
+      <Container component="main" sx={{ flexGrow: 1, p: 3, mt: 3 }}>
+        <Head>
+          <title>Our Vision</title>
+          <meta name="description" content="Learn more about SapiensLink vision." />
+        </Head>
 
-      <Typography variant="h2" gutterBottom>
-        Our Vision
-      </Typography>
+        <Typography variant="h2" gutterBottom>
+          Our Vision
+        </Typography>
 
-      <Typography variant="body1" paragraph>
-        SapiensLink main objective is to make complex educational content accessible and to empower individual creators to make their creations accessible to wider audiences without losing their ownership.
-        In order to unlock this vision, we are planning to create a space where information can consumed in different possible ways and Sapionauts can be provided with automatically generated tests to validate and prove newly acquired expertise.
-      </Typography>
+        <div style={{ textAlign: 'center' }}>
+          <img src={'media/sapiensnaut.png'} alt="architecture_system" style={{ maxWidth: "50%", display: "block", margin: "auto", marginBottom: "20px" }} />
+        </div>
 
-      <Typography variant="h4" gutterBottom>
-        Roadmap Image
-      </Typography>
-    </Container>
+        <Typography variant="body1" paragraph>
+          SapiensLink main objective is to make complex educational content accessible and to empower individual creators to make their creations accessible to wider audiences without losing their ownership.
+          In order to unlock this vision, we are planning to create a space where information can consumed in different possible ways and Sapionauts can be provided with automatically generated tests to validate and prove newly acquired expertise.
+        </Typography>
+
+        <Typography variant="h4" gutterBottom>
+          TODO: Roadmap Image
+        </Typography>
+      </Container>
     </AppLayout>
   );
 };
