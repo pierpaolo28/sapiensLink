97cb937d1cf20700a13a8cee769b87f57f5ec3b3
pierpaolo28 <40637715+pierpaolo28@users.noreply.github.com>
2024-03-16 14:24:13 +0100

    adding layout metadata

--- frontend/pages/layout.tsx
+++ frontend/pages/layout.tsx
@@ -1,29 +1,8 @@
-import type { Metadata } from "next";
+import Head from 'next/head';
 import { Inter } from "next/font/google";
 // import './globals.sass'
 
 const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "SapiensLink",
-//   description: "Sharing knowledge one link at the time.",
-//   generator: "Next.js",
-//   manifest: "/manifest.json",
-//   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
-//   authors: [
-//     { name: "Pier Paolo Ippolito" },
-//     {
-//       name: "Pier Paolo Ippolito",
-//       url: "https://ppiconsulting.dev/",
-//     },
-//   ],
-//   viewport:
-//     "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-//   icons: [
-//     { rel: "apple-touch-icon", url: "logo.svg" },
-//     { rel: "icon", url: "logo.svg" },
-//   ],
-// };
 
 export default function RootLayout({
   children,
@@ -32,8 +11,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>SapiensLink</title>
+        <meta name="description" content="Sharing knowledge one link at the time." />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
+        <meta name="generator" content="Next.js" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
 }
-
